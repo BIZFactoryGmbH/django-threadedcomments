61283c04560e3cac06a649071bb294ebca224099
Administrator <andreas.dickow@biz-factory.de>
2020-01-31 17:00:36 +0100

    changes

--- threadedcomments/admin.py
+++ threadedcomments/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy as _
-from django_comments.admin import CommentsAdmin
 
+from .compat import BASE_APP
 from .models import ThreadedComment
+
+# This code is not in the .compat module to avoid admin imports in all other code.
+# The admin import usually starts a model registration too, hence keep these imports here.
+
+if BASE_APP == 'django.contrib.comments':
+    # Django 1.7 and below
+    from django.contrib.comments.admin import CommentsAdmin
+elif BASE_APP == 'django_comments':
+    # Django 1.8 and up
+    from django_comments.admin import CommentsAdmin
+else:
+    raise NotImplementedError()
 
 
 class ThreadedCommentsAdmin(CommentsAdmin):
@@ -27,4 +39,4 @@ class ThreadedCommentsAdmin(CommentsAdmin):
                      'user_email', 'user_url', 'ip_address')
     raw_id_fields = ("parent",)
 
-admin.site.register(ThreadedComment, ThreadedCommentsAdmin)
+#admin.site.register(ThreadedComment, ThreadedCommentsAdmin)
